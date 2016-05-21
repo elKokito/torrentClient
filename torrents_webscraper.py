5daf0cc6e4d7403c2d94aabc79260dae0c622a83
kkkkk <lakdsf>
2016-05-20 21:14:45 -0400

    refactoring

--- torrents_webscraper.py
+++ torrents_webscraper.py
@@ -1,37 +1,18 @@
 from concurrent.futures import ProcessPoolExecutor
+# use ThreadPoolExecutor for dev and debug
+# ProcessPoolExecutor will deadlock in bpython (or any interpreter)
+
+from concurrent.futures import ThreadPoolExecutor
 import asyncio
 import aiohttp
 from pyquery import PyQuery as pq
 import time
-
-# global executor and loop
-executor = ProcessPoolExecutor()
-loop = asyncio.get_event_loop()
-
-def pirate_bay_parsing(body):
-    query = pq(body)
-    tbody = query("table#searchResult")[0]
-    rows = pq(tbody)
-    rows = rows("tr")
-    rows.pop(0)
-
-    results = []
-
-    for row in rows:
-        entrie = pq(row)
-        title = entrie("a.detLink").attr("title")
-        magnet = entrie("a[title='Download this torrent using magnet']").attr("href")
-        info = entrie("font.detDesc").text()
-        seeds = entrie("td[align='right']")[0].text
-        results.append({"title": title,
-                        "magnet": magnet,
-                        "info": info,
-                        "seeds": seeds})
-    return results
-
+import sys
 
 def kat_parsing(body):
+    results = []
     query = pq(body)
+    query("div.mainpart table.doublecelltable table.data.frontPageWidget")
     movies = pq(query("div.mainpart table.doublecelltable table.data.frontPageWidget")[0])
     movies_list = list(movies.find(".odd"))
     movies_list.extend(movies.find(".even"))
@@ -70,56 +51,51 @@ def kat_parsing(body):
 
     return results
 
+def pirate_bay_parsing(body):
+    query = pq(body)
+    tbody = query("table#searchResult")[0]
+    rows = pq(tbody)
+    rows = rows("tr")
+    rows.pop(0)
 
-async def kat_torrent(base_url, obj: dict):
-    body = await aiohttp.get(base_url + obj['href'])
-    body = await body.text()
-    body = pq(body)
-    magnet = body("a.kaGiantButton[title='Magnet link']").attr("href")
-    obj.update({"magnet": magnet})
+    results = []
 
-async def pipeline_piratebay(url):
-    body = await aiohttp.get(url)
-    body = await body.text()
-    result = await loop.run_in_executor(executor, pirate_bay_parsing, body)
-    return result
-
-async def pipeline_kat(url):
-    body = await aiohttp.get(url)
-    body = await body.text()
-    result = await loop.run_in_executor(executor, kat_parsing, body)
-    await asyncio.gather(*[kat_torrent(url, obj) for obj in result])
-
-    return result
-
-def launch_scapping():
-    tasks = [
-            # pirate bay tv shows
-            ("https://thepiratebay.se/top/208", pipeline_piratebay),
-            # pirate bay movies
-            ("https://thepiratebay.se/top/207", pipeline_piratebay),
-            # kickasstorrent all
-            ("http://kickasstorrentsim.com/", pipeline_kat)
-            ]
-    results = loop.run_until_complete(asyncio.gather(*[func(url) for (url, func) in tasks]))
+    for row in rows:
+        entrie = pq(row)
+        title = entrie("a.detLink").attr("title")
+        magnet = entrie("a[title='Download this torrent using magnet']").attr("href")
+        info = entrie("font.detDesc").text()
+        seeds = entrie("td[align='right']")[0].text
+        results.append({"title": title,
+                        "magnet": magnet,
+                        "info": info,
+                        "seeds": seeds})
     return results
 
 class TorrentGetter:
 
     def __init__(self):
         self.last_update = time.time()
-        self.torrents = launch_scapping()
-        self.executor = ProcessPoolExecutor()
+        self.executor = PoolExecutor()
         self.loop = asyncio.get_event_loop()
+        asyncio.set_event_loop(self.loop)
 
-    def _update_if_needed(self):
-        if time.time() - self.last_update > 60*60:
-            self.torrents = launch_scapping()
-            self.last_update = time.time()
+        self.default_tasks = [
+                # pirate bay tv shows
+                ("https://thepiratebay.se/top/208", self._pipeline_piratebay),
+                # pirate bay movies
+                ("https://thepiratebay.se/top/207", self._pipeline_piratebay),
+                # kickasstorrent all
+                ("http://kickasstorrentsim.com/", self._pipeline_kat)
+                ]
+        self.torrents = self._launch_scrapping(self.default_tasks)
 
-        def force_update(self):
-            self.torrents = launch_scapping
+    def force_update(self):
+        self.executor = PoolExecutor()
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
         self.last_update = time.time()
+        self._launch_scrapping(self.default_tasks)
 
     def get_all_torrents(self):
         self._update_if_needed()
@@ -142,7 +118,7 @@ class TorrentGetter:
         return res
 
     def search(self, query):
-        self.executor = ProcessPoolExecutor()
+        self.executor = PoolExecutor()
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
         tasks = [
@@ -152,18 +128,47 @@ class TorrentGetter:
         # return [0] just until kickasstorrent is ready
         return self._launch_scrapping(tasks)[0]
 
-    def _launch_scrapping(self, tasks):
-        results = self.loop.run_until_complete(asyncio.gather(*[func(url) for (url, func) in tasks]))
-        return results
+    def _update_if_needed(self):
+        if time.time() - self.last_update > 60*60:
+            self.loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.loop)
+            self.executor = PoolExecutor()
+            self.torrents = self._launch_scrapping(self.default_tasks)
+            self.last_update = time.time()
 
-    async def _pipekat(self, url):
-        body = await aiohttp.get(url)
+
+    async def _kat_torrent(self, base_url, obj):
+        body = await aiohttp.get(base_url + obj['href'])
         body = await body.text()
-        result = self.loop.run_in_executor(self.executor, kat_parsing, body)
-        return result
+        body = pq(body)
+        magnet = body("a.kaGiantButton[title='Magnet link']").attr("href")
+        obj.update({"magnet": magnet})
 
-    async def _pipepiratebay(self, url):
+    async def _pipeline_piratebay(self, url):
         body = await aiohttp.get(url)
         body = await body.text()
         result = await self.loop.run_in_executor(self.executor, pirate_bay_parsing, body)
         return result
+
+    async def _pipeline_kat(self, url):
+        body = await aiohttp.get(url)
+        body = await body.text()
+        result = await self.loop.run_in_executor(self.executor, kat_parsing, body)
+        await asyncio.gather(*[self._kat_torrent(url, obj) for obj in result])
+
+        return result
+
+    def _launch_scrapping(self, tasks):
+        try:
+            results = self.loop.run_until_complete(asyncio.gather(*[func(url) for (url, func) in tasks]))
+            return results[0]
+        except:
+            print('error occured in torrent webscrapper')
+            print(sys.exc_info()[0])
+
+if __name__ == "__main__":
+    PoolExecutor = ProcessPoolExecutor
+    # t = TorrentGetter()
+    # print(t.torrents)
+elif __name__ == "__console__":
+    PoolExecutor = ThreadPoolExecutor
